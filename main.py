363e5503a91f9c484de0a7c1f8bd339cccc4ce7b
hmdalvin <aafian987@gmail.com>
2022-01-01 23:15:34 +0700

    Main Frame, ScrollBar

--- main.py
+++ main.py
@@ -6,4 +6,19 @@ root = Tk()
 root.title('TextPad')
 root.geometry("1200x660")
 
+# Create Main Frame
+my_frame = Frame(root)
+my_frame.pack(pady=5)
+
+# Create our Scrollbar Fot the Text Box
+text_scroll = Scrollbar(my_frame)
+text_scroll.pack(side=RIGHT, fill=Y)
+
+# Create Text Box
+my_text = Text(my_frame, width=1200, height=660, font=("Helvetica", 16), selectbackground="yellow", selectforeground="black", undo=True, yscrollcommand=text_scroll.set)
+my_text.pack()
+
+# Configure our Scrollbar
+text_scroll.config(command=my_text.yview)
+
 root.mainloop()
