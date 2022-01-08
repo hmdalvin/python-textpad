598225d51cf0025e07c17be309f715db58bd806c
hmdalvin <aafian987@gmail.com>
2022-01-08 16:01:43 +0700

    add open function menu

--- main.py
+++ main.py
@@ -6,6 +6,35 @@ root = Tk()
 root.title('TextPad')
 root.geometry("1200x660")
 
+# Create New File Function
+def new_file():
+    # Delete previous text
+    my_text.delete("1.0", END)
+    # Update status bars
+    root.title('New File - TextPad!')
+    status_bar.config(text="New File        ")
+
+# Open Files
+def open_file():
+    # Delete previous text
+    my_text.delete("1.0", END)
+
+    # Grab Filename
+    text_file = filedialog.askopenfilename(initialdir="/home", title="Open File", filetypes=(("Text Files", "*text"), ("Html Files", "*.html"), ("Python Files", "*.py"), ("All Files", "*.*")))
+    name = text_file
+    status_bar.config(text=f'{name}       ')
+    name.replace("/Home", "")
+    root.title(f'{name} - TextPad!')
+
+    # Open the file
+    text_file = open(text_file, 'r')
+    stuff = text_file.read()
+    # Add file to textbox
+    my_text.insert(END, stuff)
+    # Close the opened file
+    text_file.close()
+
+
 # Create Main Frame
 my_frame = Frame(root)
 my_frame.pack(pady=5)
@@ -15,7 +44,7 @@ text_scroll = Scrollbar(my_frame)
 text_scroll.pack(side=RIGHT, fill=Y)
 
 # Create Text Box
-my_text = Text(my_frame, width=1200, height=660, font=("Helvetica", 16), selectbackground="yellow", selectforeground="black", undo=True, yscrollcommand=text_scroll.set)
+my_text = Text(my_frame, width=97, height=25, font=("Helvetica", 16), selectbackground="yellow", selectforeground="black", undo=True, yscrollcommand=text_scroll.set)
 my_text.pack()
 
 # Configure our Scrollbar
@@ -28,9 +57,10 @@ root.config(menu=my_menu)
 # Add File Menu
 file_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="New")
-file_menu.add_command(label="Open")
+file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="Open", command=open_file)
 file_menu.add_command(label="Save")
+file_menu.add_command(label="Save As")
 file_menu.add_separator()
 file_menu.add_command(label="Exit", command=root.quit)
 
@@ -39,6 +69,7 @@ edit_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label="Edit", menu=edit_menu)
 edit_menu.add_command(label="Cut")
 edit_menu.add_command(label="Copy")
+edit_menu.add_command(label="Paste")
 edit_menu.add_command(label="Undo")
 edit_menu.add_command(label="Redo")
 
